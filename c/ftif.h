d0a2b53816823617a4d4042d24e953ff7921f4bd
gzenner <gzenner@student.42luxembourg.lu>
2025-10-24 16:04:58 +0200

    cleam

--- c/ftif.h
+++ c/ftif.h
@@ -6,30 +6,30 @@
 /*   By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 16:36:32 by gzenner           #+#    #+#             */
-/*   Updated: 2025/07/28 16:52:54 by gzenner          ###   ########.fr       */
+/*   Updated: 2025/10/24 16:04:49 by gzenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FTIF_H
-#define FTIF_H
+# define FTIF_H
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-#include "/home/gzenner/active/libft/libft.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <string.h>
+# include "/home/gzenner/active/libft/libft.h"
 
 typedef struct s_data
 {
-	int fd;
-	char buffer[1024];
-	int bytes_read;
-    char *target;
-    char *filename;
-    unsigned int i;
-	unsigned int j;
-	unsigned int line_index;
-}   t_data;
+	unsigned int	i;
+	unsigned int	j;
+	int				fd;
+	char			*target;
+	char			*filename;
+	char			buffer[1024];
+	int				bytes_read;
+	unsigned int	line_index;
+}	t_data;
 
 #endif
