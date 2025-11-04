33edd018d2165b8fd43845ab8c6954f25c4c8d9c
gzenner <gzenner@student.42luxembourg.lu>
2025-11-04 16:12:36 +0100

    cleanup

--- c/grep.h
+++ c/grep.h
@@ -6,40 +6,26 @@
 /*   By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 09:19:28 by gzenner           #+#    #+#             */
-/*   Updated: 2025/11/03 11:21:13 by gzenner          ###   ########.fr       */
+/*   Updated: 2025/11/04 16:10:53 by gzenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GREP_H
 # define GREP_H
-
 # include <fcntl.h>
-# include <unistd.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
 # include "/home/gzenner/active/libft/libft.h"
 
 typedef struct s_data
 {
-	unsigned int	i;
-	unsigned int	j;
 	int				fd;
-	unsigned int	end;
-	char			*sub;
-	unsigned int	start;
 	char			*target;
 	char			*mbuffer;
 	char			*filename;
-	unsigned int	line_index;
 	char			buffer[1024 * 1024];
 }	t_data;
 
 void			closeonerror(int bytes_read);
-void			ft_browse_stream(t_data *data);
 void			minicalloc_char(char buffer[]);
-void			ft_print_substring(t_data *data);
 int				ft_strstr(char *haystack, char *needle);
 
 #endif
--- c/helpers.c
+++ c/helpers.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/27 08:48:31 by gzenner           #+#    #+#             */
-/*   Updated: 2025/11/03 11:20:46 by gzenner          ###   ########.fr       */
+/*   Created: 2025/11/03 11:20:12 by gzenner           #+#    #+#             */
+/*   Updated: 2025/11/04 16:12:19 by gzenner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,72 @@ void	closeonerror(int bytes_read)
 	exit(1);
 }
 
-void	ft_print_substring(t_data *data)
+void	minicalloc_char(char buffer[])
 {
-	data->sub = ft_substr(data->buffer, data->start, data->i);
-	printf("%s", data->sub);
-	free(data->sub);
-	if (data->filename)
-		printf(": (%s)", data->filename);
-	printf("\n");
+	unsigned int	i;
+
+	i = 0;
+	while (i < 1024)
+		buffer[i++] = 0;
 }
 
-void	ft_browse_stream(t_data *data)
+size_t	ft_strlen(const char *s)
 {
-	while (data->i && data->buffer[data->i - 1] != 10)
-		--data->i;
-	data->start = data->i;
-	while (data->buffer[data->i] != 10 \
-		&& data->i < ft_strlen(data->buffer))
-		++data->i;
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	if (len + 1 == 0)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len || len == 0)
+	{
+		sub = (char *)malloc(sizeof(char));
+		if (!sub)
+			return (NULL);
+		sub[0] = '\0';
+		return (sub);
+	}
+	if (s_len - start < len)
+		len = s_len - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
+	return (sub);
+}
+
+int	ft_strstr(char *haystack, char *needle)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (haystack[i])
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+			k = i;
+		while (haystack[i] == needle[j])
+		{
+			++i;
+			++j;
+		}
+		if (needle[j] == 0)
+			return (k);
+		++i;
+	}
+	return (-1);
 }
