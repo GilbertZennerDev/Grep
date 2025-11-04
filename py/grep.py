ad5bf76b283f0ff26810886ec6c8bb161c658e61
gzenner <gzenner@student.42luxembourg.lu>
2025-11-04 15:51:42 +0100

    stdin

--- py/grep.py
+++ py/grep.py
@@ -6,16 +6,25 @@
 #    By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/25 22:35:06 by pinkwish          #+#    #+#              #
-#    Updated: 2025/11/03 09:54:53 by gzenner          ###   ########.fr        #
+#    Updated: 2025/11/04 15:51:26 by gzenner          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
-def grep(filename, target):
-    content = open(filename, 'r').read().splitlines()
-    for i, line in enumerate(content):
+def grep(target, filename=None):
+    if filename is None:
+        content = sys.stdin.read().splitlines()
+    else:
+        with open(filename, 'r') as f:
+            content = f.read().splitlines()
+
+    for i, line in enumerate(content, start=1):
         if target in line:
-            print("Line", i, ":", line)
-            
-grep(sys.argv[1], sys.argv[2])
+            print(f"Line {i}: {line}")
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        grep(sys.argv[2], sys.argv[1])
+    elif len(sys.argv) == 2:
+        grep(sys.argv[1])
